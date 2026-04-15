0f70e922e5d581bcf8bd74074e7f594c465d888a
kjh4528 <jeonghyeon.kimm@gmail.com>
2026-04-15 22:55:25 +0900

    Update and rename test1.py to book_info.py
    주석 수정 및 파일 이름 변경

--- book_info.py
+++ book_info.py
@@ -1,3 +1,5 @@
+# 책 제목과 별점 크롤링하기 실습
+
 import requests
 from bs4 import BeautifulSoup
 
@@ -5,19 +7,16 @@ from bs4 import BeautifulSoup
 url = 'https://books.toscrape.com'
 res = requests.get(url)
 
-#1-1 status_code로 200(정상)인지 확인
+#1-1. status_code로 200(정상)인지 확인
 if res.status_code == 200:
     print('정상')
 
-#2. find_all()로 모든 article 먼저 수집
+#2. find_all()로 모든 article 먼저 수집 (책 정보 가져오기)
 soup = BeautifulSoup(res.text,'html.parser')
 books = soup.find_all('article',class_='product_pod')
 
 
-#3. 각 article 안의 책 제목 가져오기
-#4. 각 article 안의 별점 가져오기 (star-rating 속성을 가진 p태그 찾기)
-
-#4-1 별점 숫자로 저장
+#3. 별점을 숫자 형태로 변환하기 위한 dict 준비
 rating_dict = {
     "One":1,
     "Two":2,
@@ -26,6 +25,7 @@ rating_dict = {
     "Five":5
     }
 
+#4. 책 정보에서 책 제목과 별점만 추출하기
 book_info = []
 for book in books:
     title = book.h3.a['title']                      # 제목 추출
@@ -33,7 +33,6 @@ for book in books:
     rate_text = rate['class'][-1]                   # 별점 클래스에서 별점 정보만 가져오기 'One'
     rating = rating_dict.get(rate_text,0)           # 별점 텍스트 -> 수치로 가져오기 '1'
 
-    book_info.append({'title':title,'rating':rating})  # 책제목:별점 형태로 리스트에 저장
+    book_info.append({'title':title,'rating':rating})  # 책제목과 별점 딕셔너리로 리스트에 저장
 
-#5. 빈 list 선언해서 책 제목과 별점 저장 
 print(book_info)
